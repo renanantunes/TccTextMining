d45be01181077910c418600d3a28a1e618d66f4a
Eduardo <elopes06@gmail.com>
2014-08-21 17:03:20 -0300

    Adicionei o menu

--- TccTextMining/src/main/MainClass.java
+++ TccTextMining/src/main/MainClass.java
@@ -1,16 +1,31 @@
 package main;
 
-import arff.ARFFHandler;
+import javax.swing.JOptionPane;
+
 import twitter.TwitterListener;
 import weka.core.Instances;
+import arff.ARFFHandler;
 
 public class MainClass {
 	public static void main(String[] args) {
 		Instances data = ARFFHandler.creatARFF();
 		//ARFFHandler.saveARFF(data);
 		String keyWords[] = {"globo"};
-		TwitterListener.createLitener(keyWords, data);
-		
-	}
 
+
+		int option = -1;
+
+		do
+		{
+			System.out.println("Options:\n 1- Twitter Listener\n 2- Tokenizer\n 0- Exit");
+			option = Integer.parseInt(JOptionPane.showInputDialog("Options:\n 1- Twitter Listener\n 2- Tokenizer\n 0- Exit"));
+
+			switch(option)
+			{
+			case 1 : TwitterListener.createLitener(keyWords, data); break;
+			case 2 : break;
+			}
+
+		} while (option != 0);
+	}
 }
