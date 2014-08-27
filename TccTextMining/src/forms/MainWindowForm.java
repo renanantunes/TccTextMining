b9cdd960531d5e3732c01403ec7da02fcb17a240
Renan <renan.santunes@gmail.com>
2014-08-27 11:04:34 -0300

    commit novos files 2

---
+++ TccTextMining/src/forms/MainWindowForm.java
@@ -0,0 +1,43 @@
+package forms;
+
+public class MainWindowForm {
+	private String keyWords;
+	private String saveType;
+	private boolean isOneFilePerRecord = false;
+	private boolean isAllRecordsInOneFile = false;
+	private String path;
+	
+	public String getKeyWords() {
+		return keyWords;
+	}
+	public void setKeyWords(String keyWords) {
+		this.keyWords = keyWords;
+	}
+	public String getSaveType() {
+		return saveType;
+	}
+	public void setSaveTxt(String saveType) {
+		this.saveType = saveType;
+	}
+	public boolean isOneFilePerRecord() {
+		return isOneFilePerRecord;
+	}
+	public void setOneFilePerRecord(boolean isOneFilePerRecord) {
+		this.isOneFilePerRecord = isOneFilePerRecord;
+	}
+	public boolean isAllRecordsInOneFile() {
+		return isAllRecordsInOneFile;
+	}
+	public void setAllRecordsInOneFile(boolean isAllRecordsInOneFile) {
+		this.isAllRecordsInOneFile = isAllRecordsInOneFile;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	
+}
+
---
+++ TccTextMining/src/gui/FileChooserDirectory.java
@@ -0,0 +1,22 @@
+package gui;
+
+import javax.swing.JFileChooser;
+
+public class FileChooserDirectory {
+
+	static String initialize() {
+		
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setAcceptAllFileFilterUsed(false);
+		chooser.setCurrentDirectory(new java.io.File(""));
+		
+	
+		if(chooser.showOpenDialog(null)==JFileChooser.APPROVE_OPTION){
+			return chooser.getSelectedFile().toString();
+		}else{
+			return null;
+		}	
+	}
+
+}
