28d45f4fa74aaf8229357bdb1a4d531ba248e608
Eduardo <elopes06@gmail.com>
2014-08-31 18:20:05 -0300

    Trainer, apenas informar o path do diretorio principal aonde está as pastas pos, neg e neu que ele gera o classifier.txt

---
+++ TccTextMining/src/classifier/Trainer.java
@@ -0,0 +1,38 @@
+package classifier;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.aliasi.classify.Classification;
+import com.aliasi.classify.Classified;
+import com.aliasi.classify.DynamicLMClassifier;
+import com.aliasi.lm.NGramProcessLM;
+import com.aliasi.util.AbstractExternalizable;
+import com.aliasi.util.Files;
+
+public class Trainer 
+{
+	public void train(String dirPath) throws IOException
+	{
+		File trainDir = new File(dirPath);
+		String[] categories = trainDir.list();
+		int nGram = 7; //Aqui é usado entre 7 e 12, sabe-se lá pq
+		DynamicLMClassifier<NGramProcessLM> classifier = DynamicLMClassifier.createNGramProcess(categories, nGram);
+		
+		for(int i=0; i < categories.length; i++)
+		{
+			String category = categories[i];
+			Classification classification = new Classification(category);
+			File file = new File(trainDir, category);
+			File[] trainFiles = file.listFiles();
+			for(int j=0; j < trainFiles.length; j++)
+			{
+				File trainFile = trainFiles[j];
+				String review = Files.readFromFile(trainFile, "ISO-8859-1");
+				Classified<CharSequence> classified = new Classified<CharSequence>(review, classification);
+				classifier.handle(classified);
+			}
+		}
+		AbstractExternalizable.compileTo(classifier, new File("Train Directory\\classifier.txt"));
+	}
+}
